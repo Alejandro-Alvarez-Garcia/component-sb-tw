5fcf61f52d6f308fca0f74cdbc35ecef165010a5
Alejandro <ingeniero.alvarez.g@gmail.com>
2021-12-21 14:11:20 -0600

    Initial commmit

---
+++ components/AuroraBg.stories.js
@@ -0,0 +1,5 @@
+export default {
+  title: 'Aurora Background'
+}
+
+export const NuxtWebsite = () => '<AuroraBg />'
---
+++ components/Nav001.vue
@@ -0,0 +1,9 @@
+<template>
+<div></div>
+</template>
+
+<script>
+export default {
+  name: 'NavbarGlassm01',
+}
+</script>
